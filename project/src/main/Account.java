461d1c951eec64223ed382ba8d7f0ef8d6ed4865
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-10 23:34:38 +1100

    Move detail fields from Account to Customer
    Made Account class more generic; the business owner may not need these
    details

--- project/src/main/Account.java
+++ project/src/main/Account.java
@@ -7,42 +7,11 @@ package main;
 
 public abstract class Account
 {
-	private String firstName;
-	private String lastName;
-	
-	private String email;
-	private String phoneNumber;
-	
 	private String username;
 	
-	public Account(String firstName, String lastName, String email, String phoneNumber,
-			String username)
+	public Account(String username)
 	{
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phoneNumber = phoneNumber;
 		this.username = username;
-	}
-
-	public String getFirstName()
-	{
-		return firstName;
-	}
-
-	public String getLastName()
-	{
-		return lastName;
-	}
-
-	public String getEmail()
-	{
-		return email;
-	}
-
-	public String getPhoneNumber()
-	{
-		return phoneNumber;
 	}
 
 	public String getUsername()
--- project/src/main/Customer.java
+++ project/src/main/Customer.java
@@ -2,11 +2,39 @@ package main;
 
 public class Customer extends Account
 {
-
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	
 	public Customer(String firstName, String lastName, String email, String phoneNumber, String username)
 	{
-		super(firstName, lastName, email, phoneNumber, username);
-		// TODO Auto-generated constructor stub
+		super(username);
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
 	}
 
 }
