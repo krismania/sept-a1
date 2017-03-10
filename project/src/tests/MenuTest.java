112b478af0c9537021e5f57fc7ba9b059e0be472
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-10 17:52:50 +1100

    Create unit tests for Menu

--- project/src/tests/MenuTest.java
+++ project/src/tests/MenuTest.java
@@ -2,26 +2,42 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.Test;
-
 import main.Menu;
 
 public class MenuTest
 {
-	public String[] options = {"option 1", "option 2"};
-	public Menu testMenu = new Menu("", options);
+	public Scanner scanner;
+	public String[] options;
+	public Menu testMenu;
 
 	@Before
 	public void setUp() throws Exception
 	{
+		// This string is passed as input to the following tests. -kg
+		String inputString = "1\n";
+		scanner = new Scanner(inputString);
 		
+		options = new String[] {"first option", "second option"};
+		testMenu = new Menu(scanner, options, "Test Menu");
 	}
 
 	@Test
-	public void test()
-	{
-		fail("Not yet implemented");
+	public void testOp1()
+	{	
+		String selection = testMenu.prompt();
+		assertEquals("first option", selection);
 	}
+	
+	// Test isn't working as intended. -kg
+//	@Test
+//	public void testOp2()
+//	{	
+//		String selection = testMenu.prompt();
+//		assertEquals("second option", selection);
+//	}
 
 }
