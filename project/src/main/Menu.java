323afef043d242de97efc1f8ee877dbfacffc206
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-10 16:31:05 +1100

    Adding menu prompt functionality

--- project/src/main/Menu.java
+++ project/src/main/Menu.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 /*
  * Generic menu class, used to encapsulate menus and make
  * the rest of our solution easier. -kg
@@ -9,6 +11,8 @@ public class Menu
 {
 	private String header;
 	private String[] options;
+	
+	private Scanner sc = new Scanner(System.in);
 	
 	
 	public Menu(String header, String[] options)
@@ -20,20 +24,36 @@ public class Menu
 	
 	/*
 	 * Displays the menu, and returns the user's choice as a
-	 * string
+	 * string. -kg
 	 */
 	
 	public String prompt()
 	{
+		int userSelection; // user input
+		
 		System.out.println(header);
 		System.out.println(line(header.length()));
+		System.out.println();
+		for (int i = 0; i <= options.length; i++)
+		{
+			System.out.printf("  {0}. {1}", i+1, options[i]);
+		}
+		System.out.println();
 		
-		return "";
+		while (true)
+		{
+			System.out.print("Enter your selection: ");
+			userSelection = Integer.parseInt(sc.nextLine());
+			if (userSelection > 0 && userSelection < options.length)
+			{
+				return options[userSelection];
+			}
+		}
 	}
 	
 	
 	/*
-	 * Draws a line on the command line
+	 * Draws a line on the command line. -kg
 	 */
 	
 	private String line(int len)
