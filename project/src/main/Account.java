6a002174ba5c09186434cacbdf20375f731b94f0
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-11 09:49:03 +1100

    Fix spacing in Account

--- project/src/main/Account.java
+++ project/src/main/Account.java
@@ -9,28 +9,34 @@ public abstract class Account
 {
 	private String username;
 	
+	
 	public Account(String username)
 	{
 		this.username = username;
 	}
 
+	
 	public String getUsername()
 	{
 		return username;
 	}
 	
+	
 	/*
 	 * Test the given username/password combination against stored accounts. -kg
 	 */
+	
 	public static boolean attemptLogin(String username, String password)
 	{
 		//TODO: Implement login -kg
 		return false;
 	}
 	
+	
 	/*
 	 * Add an account with the provided username/password to storage. -kg
 	 */
+	
 	public static boolean createAccount(String username, String password)
 	{
 		//TODO: Implement account creation -kg
