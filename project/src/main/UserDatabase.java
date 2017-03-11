42102064f0d5a6845a453924e323c7568a266037
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-03-11 21:49:02 +1100

    Added functionality to create table
    Added create table function

--- project/src/main/UserDatabase.java
+++ project/src/main/UserDatabase.java
@@ -1,14 +1,18 @@
 package main;
 import java.sql.*;
 
-//JM User Database implementation. Currently just CREATES a DB File.
-
+/* JM User Database implementation. 
+ * Loads database connection to that specified in line 19. Will then invoke 
+ * scripts to create users automatically
+*/
 public class UserDatabase {
+	Connection c = null;
+	Statement stmt = null;
+	
 	public void CreateDatabase()
 	{
 		//JM Initialize a connection
-		Connection c = null;
-		System.out.println("Attempting to connect to the database");
+		System.out.println("Attempting to connect to the database...");
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
@@ -22,5 +26,29 @@ public class UserDatabase {
 		}
 		//JM Success message means DB is found, or created.
 		System.out.println("Opened database successfully");
+		CreateDatabaseTable();
+		System.out.println();
+	}
+	
+	public void CreateDatabaseTable()
+	{
+		System.out.println("Creating table in Database...");
+		try {
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE CUSTOMERS " +
+						 "(FirstName varchar(255)," +
+						 "LastName varchar (255))";
+			
+			stmt.executeUpdate(sql);
+			System.out.println("Created table in Database!");
+		} catch (SQLException e) {
+			//JM Handles errors for JDBC
+			e.printStackTrace();
+		} catch (Exception e) {
+			//JM Handles errors for Class.forName
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
