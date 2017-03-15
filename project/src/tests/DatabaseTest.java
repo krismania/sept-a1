d2c90d2b71c3b05320dc6909cc23846c5f7fe3ed
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-03-16 07:10:18 +1100

    Began setup for testing DB.

---
+++ project/src/tests/DatabaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.UserDatabase;
+
+public class DatabaseTest {
+
+	public UserDatabase db;
+
+	@Before
+	public void setUp() throws Exception {
+		//db = new UserDatabase();
+		//db.CreateDatabase();
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
