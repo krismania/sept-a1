131a2009c2621b7961296ba11ad9ea327e8460e9
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-17 22:56:06 +1100

    Add alert method to Console

--- project/src/console/Console.java
+++ project/src/console/Console.java
@@ -5,12 +5,31 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * Handles the applications interaction with the console. Any input
+ * collected from the user, as well as any output printed to them, should go
+ * through this class.
+ * @author krismania
+ */
+
 public class Console
 {
 	private Scanner sc;
 	
+	
 	public Console(Scanner sc)
 	{
 		this.sc = sc;
+	}
+	
+	
+	/**
+	 * Prints an alert message to the console, with a trailing line for spacing.
+	 * @param message The message to print
+	 */
+	
+	public void alert(String message)
+	{
+		System.out.println(message + "\n");
 	}
 }
