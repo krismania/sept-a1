3686398c7ff77089ef76dc028f35a4bb16093acc
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-17 22:56:47 +1100

    Add prompts to Console

--- project/src/console/Console.java
+++ project/src/console/Console.java
@@ -32,4 +32,61 @@ public class Console
 	{
 		System.out.println(message + "\n");
 	}
+	
+	
+	/**
+	 * Sequentially displays each item in {@code fields}, prompting the user for
+	 * an input with each field.
+	 * @param fields A {@link LinkedHashMap} containing value/prompt pairs; for
+	 * example, {@code <"firstName", "First Name">}.
+	 * @return {@link HashMap} containing the keys from {@code fields}, paired
+	 * with the users responses to them.
+	 */
+	
+	private HashMap<String, String> prompt(LinkedHashMap<String, String> fields)
+	{
+		HashMap<String, String> input = new HashMap<String, String>();
+		
+		for (Map.Entry<String, String> field : fields.entrySet())
+		{
+			System.out.print(field.getValue() + ": ");
+			input.put(field.getKey(), sc.nextLine());
+		}
+		
+		System.out.println(); // add space under last field -kg
+		return input;
+	}
+	
+	
+	/**
+	 * Prompts the user for a username & password using {@link #prompt(LinkedHashMap) prompt}.
+	 * @return {@link HashMap} containing the keys {@code "username"} and {@code "password"}.
+	 */
+	
+	public HashMap<String, String> accountPrompt()
+	{
+		LinkedHashMap<String, String> fields = new LinkedHashMap<String, String>();
+		fields.put("username", "Username");
+		fields.put("password", "Password");
+		
+		return prompt(fields);
+	}
+	
+	
+	/**
+	 * Prompts the user for customer information using {@link #prompt(LinkedHashMap) prompt}.
+	 * @return {@link HashMap} containing the keys {@code "firstName"}, {@code "lastName"},
+	 * {@code "email"} and {@code "phoneNumber"}.
+	 */
+	
+	public HashMap<String, String> accountInfoPrompt()
+	{
+		LinkedHashMap<String, String> fields = new LinkedHashMap<String, String>();
+		fields.put("firstName", "First Name");
+		fields.put("lastName", "Last Name");
+		fields.put("email", "Email Address");
+		fields.put("phoneNumber", "Contact Number");
+		
+		return prompt(fields);
+	}
 }
