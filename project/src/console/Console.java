e6f4b76fc16c0d95d462d0a6ecea48a1dac14e62
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-03-17 22:24:09 +1100

    Create class Console

---
+++ project/src/console/Console.java
@@ -0,0 +1,16 @@
+package console;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Console
+{
+	private Scanner sc;
+	
+	public Console(Scanner sc)
+	{
+		this.sc = sc;
+	}
+}
