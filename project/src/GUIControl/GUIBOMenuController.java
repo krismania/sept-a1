5af4adc75827a4cac412ea73f0791d14467381ee
tn <tn@sirs-corner>
2017-04-04 20:47:16 +1000

    TN - IMplemented Transitions for Main Business Owner Menu

--- project/src/GUIControl/GUIBOMenuController.java
+++ project/src/GUIControl/GUIBOMenuController.java
@@ -5,9 +5,18 @@
  */
 package GUIControl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 
 /**
  * FXML Controller class
@@ -16,9 +25,40 @@ import javafx.fxml.Initializable;
  */
 public class GUIBOMenuController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button addEmp;
+
+    @FXML
+    private Button addShift;
+
+    @FXML
+    private Button viewBooking;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        if(event.getSource()==addEmp) {
+        	//TN - get reference button stage
+        	stage=(Stage) addEmp.getScene().getWindow();
+        	//TN - load other scene
+        	root = FXMLLoader.load(getClass().getResource("GUIAddEmployee.fxml"));
+        }
+        else if(event.getSource()==addShift) {
+        	stage=(Stage) addShift.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("GUIAddShift.fxml"));
+        }
+        else
+        {
+        	stage=(Stage) viewBooking.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("GUIBOViewBooking.fxml"));
+        }
+        //TN - call a new scene instance
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
