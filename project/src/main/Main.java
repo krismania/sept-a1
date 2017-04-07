ef2e17ea7360d0e785f357fef84e1425dd66fb46
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-04-07 18:23:43 +1000

    Refactor Console code
    Moved all of the interface code into Console - the controller will only
    handle data, and not any interface flow

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,11 +1,16 @@
 package main;
+import java.util.Scanner;
+
+import console.Console;
 
 public class Main
 {  
 	
 	public static void main(String[] args)
 	{
-		Controller abs = Controller.getInstance();
-		abs.mainMenu();
+		Scanner sc = new Scanner(System.in);
+		Console console = new Console(sc);
+		
+		console.start();
 	}
 }
