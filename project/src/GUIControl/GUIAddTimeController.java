3cfe058625639e356e9958de56750e47dbde2613
Riger73 <s3572290@student.rmit.edu.au>
2017-04-07 10:50:41 +1000

    TN -  Implement AddTime

--- project/src/GUIControl/GUIAddTimeController.java
+++ project/src/GUIControl/GUIAddTimeController.java
@@ -5,9 +5,19 @@
  */
 package GUIControl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 
 /**
  * FXML Controller class
@@ -16,9 +26,40 @@ import javafx.fxml.Initializable;
  */
 public class GUIAddTimeController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btRecordAvail;
+
+    @FXML
+	private Button exit;
+ 
+    @FXML
+	private Button menu;
+
+	@FXML
+	void handleButtonAction(ActionEvent event) throws IOException{
+        Stage stage;
+        Parent root;
+        if(event.getSource()==btRecordAvail) {
+            //TN - Need to impliment capture data and send to method
+        	
+        	//TN - Directs to confirmation scene
+        	stage=(Stage) btRecordAvail.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("GUIAddShift.fxml"));
+            
+        }
+        else
+        {
+            stage=(Stage) menu.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("GUIBOMenu.fxml"));
+        }
+    }
+	
+    @FXML
+    //TN - Button to close app.
+    private void closeButtonAction(ActionEvent event) throws IOException {
+    	Stage stage = (Stage) exit.getScene().getWindow();
+        stage.close();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
