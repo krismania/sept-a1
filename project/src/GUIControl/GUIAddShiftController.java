0b2894d2f23f51a9464fb9c3f698d3ff71accefc
Riger73 <s3572290@student.rmit.edu.au>
2017-04-07 10:47:32 +1000

    TN - Implement AddShift

--- project/src/GUIControl/GUIAddShiftController.java
+++ project/src/GUIControl/GUIAddShiftController.java
@@ -5,20 +5,43 @@
  */
 package GUIControl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 
-/**
- * FXML Controller class
- *
- * @author tn
- */
+
+
 public class GUIAddShiftController implements Initializable {
+	
+	@FXML
+	private Button exit;
 
-    /**
-     * Initializes the controller class.
-     */
+	@FXML
+	private Button menu;
+    
+	@FXML
+	void handleButtonAction(ActionEvent event) throws IOException{
+    Stage stage;
+    Parent root;		
+    stage=(Stage) menu.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("GUIBOMenu.fxml"));
+    
+	}
+	@FXML
+    private void closeButtonAction(ActionEvent event) throws IOException {
+    	Stage stage = (Stage) exit.getScene().getWindow();
+        stage.close();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
