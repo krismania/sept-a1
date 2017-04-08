5bb815eee893bdad671d41ade8a9064026c14297
Riger73 <s3572290@student.rmit.edu.au>
2017-04-09 00:00:15 +1000

    TN - had to fix getters for Employee constructor to put ID first

--- project/src/main/Database.java
+++ project/src/main/Database.java
@@ -78,7 +78,7 @@ public class Database implements DBInterface {
 		// create the object and return it
 		int id = currentHighestID + 1;
 		
-		return new Employee("", "", "", "", id);
+		return new Employee(id,"", "", "", "");
 	}
   
 	@Override
@@ -311,7 +311,7 @@ public class Database implements DBInterface {
 			        String email = rs.getString("Email");
 			        String phone = rs.getString("Phone");
 			        closeConnection();
-			        return new Employee(first, last, email, phone, id);
+			        return new Employee(id, first, last, email, phone);
 				}
 			}
 			
@@ -344,7 +344,7 @@ public class Database implements DBInterface {
 			        String phone = rs.getString("Phone");
 			        int EmpID = rs.getInt("EmpID");
 			        
-			        Employee current = new Employee(first, last, email, phone, EmpID);
+			        Employee current = new Employee(EmpID, first, last, email, phone);
 			        
 			        roster.add(current);
 				}
