c7362995c39476015a60a3518d97c58b666d3ec2
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-04-08 17:16:37 +1000

    Sort shifts by day and time

--- project/src/console/Console.java
+++ project/src/console/Console.java
@@ -5,6 +5,7 @@ import java.sql.Time;
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -384,25 +385,56 @@ public class Console
 	 */
 	private void displayShifts(ArrayList<Shift> shifts)
 	{
-		for(Shift current : timeSlots)
+		if(shifts.isEmpty())
 		{
-			int shiftID = current.ID;
-			int empID = current.employeeID;
-			DayOfWeek day = current.getDay();
-			ShiftTime time = current.getTime();
-			Locale locale = new Locale("au", "AU");
-			System.out.println("***");
-			System.out.println("Option: " + Integer.toString(shiftID));
-			System.out.println("Employee: " + Integer.toString(empID) 
-					+ ", Day: " + day.getDisplayName(TextStyle.SHORT, locale) 
-					+ ", Time: " + time.toString());
+			alert("There are currently no available timeslots.");
+			return;
+		}
+		
+		// sort the shifts by day
+		Comparator<Shift> byDayAndTime = new Comparator<Shift>()
+		{
+			@Override
+			public int compare(Shift s1, Shift s2)
+			{
+				// sort on day
+				int byDay = s1.getDay().getValue() - s2.getDay().getValue();
+				
+				// if same day, compare time
+				if (byDay == 0)
+				{
+					return s1.getTime().getValue() - s2.getTime().getValue();
+				}
+				else
+				{
+					return byDay;
+				}
+			}
+		};
+		
+		//Comparator<Shift> byDay = ((s1, s2) -> s1.getDay().getValue() - s2.getDay().getValue());
+		
+		logger.info("Sorting shift list");
+		shifts.sort(byDayAndTime);
+		
+		for (Shift shift : shifts)
+		{
+			System.out.println(shift);
+			
+//			int shiftID = current.ID;
+//			int empID = current.employeeID;
+//			DayOfWeek day = current.getDay();
+//			ShiftTime time = current.getTime();
+//			Locale locale = new Locale("au", "AU");
+//			System.out.println("***");
+//			System.out.println("Option: " + Integer.toString(shiftID));
+//			System.out.println("Employee: " + Integer.toString(empID) 
+//					+ ", Day: " + day.getDisplayName(TextStyle.SHORT, locale) 
+//					+ ", Time: " + time.toString());
 		}
 		System.out.println();
 		
-		if(timeSlots.isEmpty())
-		{
-			alert("There are currently no available timeslots.");
-		}
+		
 	}
 	
 	// **** CLASS FUNCTIONALITY ****
