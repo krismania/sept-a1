480e55621c5e5f2270dd6ed91a38aa1138cfdb79
Kristian Giglia <s3543819@student.rmit.edu.au>
2017-04-09 19:19:41 +1000

    fix controller tests

--- project/src/tests/ControllerTest.java
+++ project/src/tests/ControllerTest.java
@@ -9,62 +9,69 @@ import main.Controller;
 
 public class ControllerTest
 {
+	public Controller controller;
+	
+	@Before
+	public void setUp()
+	{
+		controller = Controller.getInstance();
+	}
 
 	// test static validator methods. -kg	
 	@Test
 	public void testValidName()
 	{
-		assertTrue(Controller.validateName("John"));
+		assertTrue(controller.validateName("John"));
 	}
 	
 	@Test
 	public void testInvalidName()
 	{
-		assertFalse(Controller.validateName(""));
+		assertFalse(controller.validateName(""));
 	}
 	
 	@Test
 	public void testValidEmail()
 	{
-		assertTrue(Controller.validateEmail("some-email@gmail.com"));
+		assertTrue(controller.validateEmail("some-email@gmail.com"));
 	}
 	
 	@Test
 	public void testInvalidEmail1()
 	{
 		// no @ symbol
-		assertFalse(Controller.validateEmail("myemail.website.com"));
+		assertFalse(controller.validateEmail("myemail.website.com"));
 	}
 	
 	@Test
 	public void testInvalidEmail2()
 	{
 		// no . symbol
-		assertFalse(Controller.validateEmail("emailwithout@dots"));
+		assertFalse(controller.validateEmail("emailwithout@dots"));
 	}
 	
 	@Test
 	public void testInvalidEmail3()
 	{
 		// no . or @ symbols
-		assertFalse(Controller.validateEmail("thisisdefinitelynotanemail"));
+		assertFalse(controller.validateEmail("thisisdefinitelynotanemail"));
 	}
 	
 	// @author - RK
 	@Test
 	public void testInvalidEmail4(){
-		assertFalse(Controller.validateEmail("richard@kuoch@"));
+		assertFalse(controller.validateEmail("richard@kuoch@"));
 	}
 	
 	//@author -RK
 	@Test
-	
 	public void testPhoneNumber(){
-		assertFalse(Controller.validatePhoneNumber(""));
+		assertFalse(controller.validatePhoneNumber(""));
 	}
 	
+	@Test
 	public void testPhoneNumber2(){
-		assertTrue(Controller.validatePhoneNumber("98029744"));
+		assertTrue(controller.validatePhoneNumber("98029744"));
 	}
 	
 }
