300b75313cb4916b5e5ca15c97566c381ef6674d
Kristian Giglia <krismania1@gmail.com>
2017-04-24 13:22:20 +1000

    Rebuild login ui / controller
    Added a new version of the Login interface and controller, fixing issues with crashing and also updating the look

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application
 	     
     @Override 
     public void start(Stage stage) throws Exception { 
-        Parent root = FXMLLoader.load(getClass().getResource("/GUIControl/GUILoginPopup.fxml")); 
+        Parent root = FXMLLoader.load(getClass().getResource("/GUIControl/Login.fxml")); 
          
         Scene scene = new Scene(root); 
          
