8beb54255d69eb4aecb94c31cb71abbc95fb92c2
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-04-27 19:37:52 +1000

    Push skeleton for Booking Scene

---
+++ project/src/GUIControl/GUIBookingController.java
@@ -0,0 +1,19 @@
+package GUIControl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class GUIBookingController {
+
+}
