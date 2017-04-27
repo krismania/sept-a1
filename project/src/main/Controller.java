4f500a977e12f546e87007e04c90d050e9ea1504
Kristian Giglia <krismania1@gmail.com>
2017-04-27 16:46:16 +1000

    Move pw validation into Controller
    Makes validation more consistent as all other validation is there.
    
    Also changed the tests & 1 use in the GUI code to point to the new location

--- project/src/main/Controller.java
+++ project/src/main/Controller.java
@@ -241,4 +241,34 @@ public class Controller
 		input = input.replaceAll("[ \\-.()]", "");
 		return input.matches("\\+?(\\d{8}|\\d{10,11})");
 	}
+	
+	/**
+	 * Validates account passwords based on some rules
+	 * TODO: document the rules
+	 * @author krismania
+	 */
+	public boolean passwordAccepted(String password)
+	{
+		//check length
+		if (password.length() >= 6 && password.length() < 15 && password.matches("[a-zA-Z0-9]+"))
+		{
+			// loop through each character and check if there is (at least) 1 upper case, 1 lower case and 1 number
+			// the following vars are set to true once an occurrence of each is found. -kg
+			boolean upper = false;
+			boolean lower = false;
+			boolean num = false;
+			
+			for (int i = 0; i < password.length(); i++)
+			{
+				// the surrounding if statements prevent extra checks once criteria have been met. -kg
+				if (!upper) if (Character.isUpperCase(password.charAt(i))) upper = true;
+				if (!lower) if (Character.isLowerCase(password.charAt(i))) lower = true;
+				if (!num) if (Character.isDigit(password.charAt(i))) num = true;
+			}
+			
+			// return true if all criteria are met. -kg
+			return upper && lower && num;
+		}
+		return false;
+	}
 }
