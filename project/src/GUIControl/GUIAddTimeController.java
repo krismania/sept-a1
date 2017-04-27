011b0a996b5c77d5674573dcf7084a7882f82758
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-04-27 20:19:42 +1000

    fixed am time option

--- project/src/GUIControl/GUIAddTimeController.java
+++ project/src/GUIControl/GUIAddTimeController.java
@@ -93,7 +93,7 @@ public class GUIAddTimeController implements Initializable {
     	//TN - initialise shift time slot dropdown menus
     	shiftDropdown.getItems().removeAll(shiftDropdown.getItems());
         shiftDropdown.getItems().addAll("9:00 am", "9:30 am", "10:00 am", "10:30 am", 
-        		"11:00 am", "11:30 pm", "12:00 pm", "12:30 pm", "1:00 pm", "1:30 pm", 
+        		"11:00 am", "11:30 am", "12:00 pm", "12:30 pm", "1:00 pm", "1:30 pm", 
         		"2:00 pm", "2:30 pm", "3:00 pm", "3:30 pm", "4:00 pm", "4:30pm", "5:00 pm");
         shiftDropdown.getSelectionModel().select("9:00 am");
         
