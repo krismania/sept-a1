ea13e7f3a68a71fbc6c3647d7a046b6c8efe17a0
Kristian Giglia <krismania1@gmail.com>
2017-04-28 00:06:00 +1000

    Clean up AddEmployee controller code

--- project/src/GUIControl/AddEmployeeController.java
+++ project/src/GUIControl/AddEmployeeController.java
@@ -1,112 +1,144 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUIControl;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.ResourceBundle;
 import javafx.stage.Stage;
 import main.Controller;
-import javafx.scene.Parent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author tn
+ * @author tn, krismania
  */
-public class AddEmployeeController implements Initializable {  
+public class AddEmployeeController
+{  
     Controller c = Controller.getInstance();
 	
-    @FXML 
-    private Label lblError;
+    @FXML private BorderPane root;
     
-    @FXML
-    private TextField tfEmpFName;
-
-    @FXML
-    private TextField tfEmpLName;
-
-    @FXML
-    private TextField tfEmpEmailAdd;
-
-    @FXML
-    private TextField tfEmpPhNum;
-
-    @FXML
-    private Button submitNewEmpData;
+    @FXML private Label lblError;
+    @FXML private TextField tfEmpFName;
+    @FXML private TextField tfEmpLName;
+    @FXML private TextField tfEmpEmailAdd;
+    @FXML private TextField tfEmpPhNum;
+    @FXML private Button submitNewEmpData;
+    @FXML private Button navMenu;
+    @FXML private Button exit;
     
-    @FXML
-    private Button navMenu;
-    
-    @FXML
-    private Button exit;
+    /**
+     * Switches to a specified scene
+     * @author krismania
+     */
+    private void switchTo(String fxmlName)
+	{
+		try
+		{
+			// load the scene
+			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
+			
+			// get current stage
+			Stage stage = (Stage) root.getScene().getWindow();
+			
+			// switch scenes
+			stage.setScene(newScene);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Could not switch scene.");
+			e.printStackTrace();
+		}
+	}
 
     @FXML
-    void handleButtonAction(ActionEvent event) throws IOException {
-    	//Controller.getInstance().getAllEmployees();
-    	String firstName = tfEmpFName.getText();
-    	String lastName = tfEmpLName.getText();
-    	String email = tfEmpEmailAdd.getText();
-    	String phoneNumber = tfEmpPhNum.getText();
-    	employee(firstName, lastName, email, phoneNumber);
-    }
-
-    @FXML
-    private void closeButtonAction(ActionEvent event) throws IOException {
-    	Stage stage = (Stage) exit.getScene().getWindow();
-        stage.close();
-    }
-    @FXML
-    private void navMenuButtonAction(ActionEvent event) throws IOException {
-    	Stage stage = (Stage) navMenu.getScene().getWindow();
-        // load the scene
-        Scene boMenu = new Scene(FXMLLoader.load(getClass().getResource("GUIBOMenu.fxml")));
-        // switch scenes
-        stage.setScene(boMenu);
-    }
-    
-    //TN - Gathers Employee variables and returns a boolean for validation of field processing
-    private boolean employee(String firstName, String lastName, String email, String phoneNumber){
-    	//TN - Add text field input to Controller addEmployee method
-    	boolean addEmp = c.addEmployee(firstName, lastName, email, phoneNumber);
-    	if(addEmp == false)
-        {
-            //TN - Presents red error message if input is false
-            lblError.setVisible(true);
-            return false;
-        }
-        else 
+    public void handleAddEmployee(ActionEvent event)
+    {
+    	if (validateEmployee())
     	{
-            //TN -  If validation is successful a confirmation popup is presented.
-            GUIAlert info  = new GUIAlert();
-            lblError.setVisible(false);
-            info.infoBox("New Employee Successfully Added", "Add Employee Confirmation");
-            //TN - Fields are cleared following correct input.
-            tfEmpFName.clear();
-            tfEmpLName.clear(); 
-            tfEmpEmailAdd.clear();
-            tfEmpPhNum.clear();
-            return true;
+    		// try to add the employee
+    		if (c.addEmployee(tfEmpFName.getText(), tfEmpLName.getText(), tfEmpEmailAdd.getText(), tfEmpPhNum.getText()))
+    		{
+    			// employee added successfully
+    			//TN -  If validation is successful a confirmation popup is presented.
+                lblError.setVisible(false);
+                GUIAlert.infoBox("New Employee Successfully Added", "Add Employee Confirmation");
+                
+                //TN - Fields are cleared following correct input.
+                tfEmpFName.clear();
+                tfEmpLName.clear(); 
+                tfEmpEmailAdd.clear();
+                tfEmpPhNum.clear();
+                
+                // set focus to the first field. -kg
+                tfEmpFName.requestFocus();
+    		}
+    		else
+    		{
+    			setError("Couldn't add an employee.");
+    		}
     	}
-    } 	
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    }
+    
+    @FXML
+    public void handleBack(ActionEvent event)
+    {
+    	switchTo("GUIBOMenu.fxml");
+    }
+    
+    /**
+     * @author krismania
+     */
+    private void setError(String message)
+    {
+    	lblError.setText(message);
+    	lblError.setVisible(true);
+    }
+    
+    /**
+     * Returns true if all fields are valid, otherwise returns false and displays
+     * a message in the GUI.
+     * TODO: this looks kind of gross, fix it
+     * @author krismania
+     */
+    private boolean validateEmployee()
+    {
+    	if (c.validateName(tfEmpFName.getText()))
+    	{
+    		if (c.validateName(tfEmpLName.getText()))
+    		{
+    			if (c.validateEmail(tfEmpEmailAdd.getText()))
+    			{
+    				if (c.validatePhoneNumber(tfEmpPhNum.getText()))
+    				{
+    					lblError.setVisible(false);
+    					return true;
+    				}
+    				else
+    				{
+    					setError("Please enter a valid phone number");
+    					// TODO: this message is too long
+    				}
+    			}
+    			else
+    			{
+    				setError("Please enter a valid email");
+    			}
+    		}
+    		else
+    		{
+    			setError("Please enter a last name");
+    		}
+    	}
+    	else
+    	{
+    		setError("Please enter a first name");
+    	}
+    	return false;
+    }
 }
