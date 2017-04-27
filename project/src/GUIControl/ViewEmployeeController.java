a2cc60ad9d63e941aef818d8ad9099354ffc6e4a
Kristian Giglia <krismania1@gmail.com>
2017-04-27 22:14:22 +1000

    Render employee data in TableView
    Detailed information is also displayed in the details pane (on the right)  for the selected employee

--- project/src/GUIControl/ViewEmployeeController.java
+++ project/src/GUIControl/ViewEmployeeController.java
@@ -1,16 +1,26 @@
 package GUIControl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 import main.Controller;
 import main.Employee;
 
@@ -21,17 +31,79 @@ public class ViewEmployeeController implements Initializable
 	@FXML private BorderPane root;
 	
 	@FXML private TableView<Employee> employeeTable;
-	@FXML private TableColumn<Employee, String> employeeID;
+	@FXML private TableColumn<Employee, Number> employeeID;
 	@FXML private TableColumn<Employee, String> employeeName;
+
+	@FXML private TextField selEmpID;
+	@FXML private TextField selEmpFirstName;
+	@FXML private TextField selEmpLastName;
+	@FXML private TextField selEmpEmail;
+	@FXML private TextField selEmpPhone;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-		employeeID.setCellValueFactory(new PropertyValueFactory<Employee, String>("id"));
+		// observe changes to selection
+		
+		// populate employee list
+		employeeID.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Employee, Number>, ObservableValue<Number>>() {
+
+			@Override
+			public ObservableValue<Number> call(
+							CellDataFeatures<Employee, Number> param)
+			{
+				IntegerProperty prop = new SimpleIntegerProperty();
+				prop.setValue(param.getValue().ID);
+				
+				return prop;
+			}
+			
+		});
+		
+		// employeeID.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("ID"));
 		employeeName.setCellValueFactory(new PropertyValueFactory<Employee, String>("firstName"));
 		
 		employeeTable.getItems().setAll(c.getAllEmployees());
 	}
 	
+	private void switchTo(String fxmlName)
+	{
+		try
+		{
+			// load the scene
+			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
+			
+			// get current stage
+			Stage stage = (Stage) root.getScene().getWindow();
+			
+			// switch scenes
+			stage.setScene(newScene);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Could not switch scene.");
+			e.printStackTrace();
+		}
+	}
 	
+	@FXML
+	public void handleChangeEmployee(MouseEvent event)
+	{
+		Employee selEmp = employeeTable.getSelectionModel().getSelectedItem();
+		
+		System.out.println(selEmp.ID);
+		
+		// show employee details in details pane
+		selEmpID.setText(Integer.toString(selEmp.ID));
+		selEmpFirstName.setText(selEmp.getFirstName());
+		selEmpLastName.setText(selEmp.getLastName());
+		selEmpEmail.setText(selEmp.getEmail());
+		selEmpPhone.setText(selEmp.getPhoneNumber());
+	}
+	
+	@FXML
+	public void handleBack(ActionEvent event)
+	{
+		switchTo("GUIBOMenu");
+	}
 }
