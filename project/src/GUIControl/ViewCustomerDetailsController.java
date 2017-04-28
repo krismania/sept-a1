b26019bc10d200af5711ebe6796cd3251f9745d0
Kristian Giglia <krismania1@gmail.com>
2017-04-28 22:26:14 +1000

    Add customer detail screen
    Also added a menu item in customer's menu.

---
+++ project/src/GUIControl/ViewCustomerDetailsController.java
@@ -0,0 +1,75 @@
+package GUIControl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.Controller;
+
+/**
+ * Displays the logged in customer's details.
+ * @author krismania
+ */
+public class ViewCustomerDetailsController implements Initializable
+{
+	private Controller c = Controller.getInstance();
+	
+	@FXML private Node root;
+	
+	@FXML private Label username;
+	@FXML private TextField firstName;
+	@FXML private TextField lastName;
+	@FXML private TextField email;
+	@FXML private TextField phone;
+	
+	
+	/**
+     * Switches to a specified scene
+     * @author krismania
+     */
+    private void switchTo(String fxmlName)
+	{
+		try
+		{
+			// load the scene
+			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
+			
+			// get current stage
+			Stage stage = (Stage) root.getScene().getWindow();
+			
+			// switch scenes
+			stage.setScene(newScene);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Could not switch scene.");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		// get user's account objects
+		//c.getCustomer(c.loggedUser);
+		
+		// populate the fields
+		username.setText("MyUsername");
+	}
+	
+	@FXML
+	public void handleBack(ActionEvent event)
+	{
+		switchTo("GUICustMenu");
+	}
+	
+}
