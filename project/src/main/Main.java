d64cfa1cf5c8ceecdba51dea6878b697b14723b9
Kristian Giglia <krismania1@gmail.com>
2017-04-29 09:35:26 +1000

    Remove old console launch code

--- project/src/main/Main.java
+++ project/src/main/Main.java
@@ -1,6 +1,4 @@
 package main;
-import java.util.Scanner;
-import console.Console;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,38 +7,21 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {  
-	     
     @Override 
-    public void start(Stage stage) throws Exception { 
+    public void start(Stage stage) throws Exception
+    { 
         Parent root = FXMLLoader.load(getClass().getResource("/GUIControl/Login.fxml")); 
          
         Scene scene = new Scene(root); 
          
-        stage.setResizable(false);
+        stage.setResizable(false); //TODO: support resizing
         stage.setScene(scene); 
         stage.setTitle("Appointment Booking System");
         stage.show(); 
-    } 
- 
-    /** 
-     * @param args the command line arguments 
-     */ 
-    public static void main(String[] args) { 
+    }
+    
+    public static void main(String[] args)
+    { 
         launch(args); 
-    } 
-	
-    
-    // console launch. -kg
-    
-//	public static void main(String[] args)
-//	{
-//		if (args.length == 1 && args[0].equals("-debugDB"))
-//		{
-//			Controller.debugDB = true;
-//		}
-//		Scanner sc = new Scanner(System.in);
-//		Console console = new Console(sc);
-//		
-//		console.start();
-//	}
+    }
 }
