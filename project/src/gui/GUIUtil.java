7dec014fd3be83c8d9c87e28c3a13aaa15bd18ff
Kristian Giglia <krismania1@gmail.com>
2017-05-01 09:48:11 +1000

    Add switchTo method to GUIUtil

--- project/src/gui/GUIUtil.java
+++ project/src/gui/GUIUtil.java
@@ -1,5 +1,12 @@
 package gui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Utility class for common GUI functionality
  * @author krismania
@@ -8,5 +15,32 @@ public final class GUIUtil
 {
 	private GUIUtil() {} // prevents instantiation
 	
+	/**
+	 * Application root node, should be set on launch. the switchTo method uses
+	 * this node to switch scenes.
+	 */
+	public static Node root;
 	
+	/**
+     * Generic Helper Method for Switching to specified scenes
+     * @author krismania
+     */
+    public void switchTo(String fxmlName)
+	{
+		try
+		{
+			// load the scene
+			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
+			
+			// get current stage
+			Stage stage = (Stage) root.getScene().getWindow();
+			
+			// switch scenes
+			stage.setScene(newScene);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
