7d281138a981e2cd2c0ad000497cc819c567b1ff
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-05-05 21:15:29 +1000

    Initialise dropdown menu on signup

--- project/src/gui/SignupController.java
+++ project/src/gui/SignupController.java
@@ -1,22 +1,27 @@
 package gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import main.Controller;
 import main.Validate;
+
 //Implements Signup form for people without existing login details
-public class SignupController
+public class SignupController implements Initializable
 {
 	private Controller c = Controller.getInstance();
 	
@@ -32,6 +37,8 @@ public class SignupController
 	
 	@FXML private Label lblError1;
 	@FXML private Label lblError2;
+	
+	@FXML private ChoiceBox<String> businessPicker;
 	
 	//Helper method for triggering error output
 	private void setError(Label label, String text)
@@ -185,5 +192,12 @@ public class SignupController
 				setError(lblError2, "Account already exists.");
 			}
 		}
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb)
+	{
+		businessPicker.getItems().removeAll(businessPicker.getItems());
+		businessPicker.getItems().addAll(c.getAllBusinessNames());
 	}
 }
