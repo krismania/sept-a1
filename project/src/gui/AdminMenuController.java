2bfe9e56f20aef801b0c927b03935b625a12ca33
rmit-s3563242-richard-kuoch <s3563242@student.rmit.edu.au>
2017-05-05 19:33:05 +1000

    Create AdminMenuController.java

---
+++ project/src/gui/AdminMenuController.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.stage.Stage;
+import main.Controller;
+import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+/**
+ * Implements Admin Main Menu options selections
+ *
+ * @author RK
+ */
+public class AdminMenuController implements Initializable 
+{
+	
+	private Controller c = Controller.getInstance();
+	
+	 @Override
+	 public void initialize(URL url, ResourceBundle rb) 
+	 {
+	        // INIT
+	 }   
+	
+}
