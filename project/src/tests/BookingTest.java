5fc0cd39d52d89ff45590e0f921d843e4cd2efa5
Kristian Giglia <krismania1@gmail.com>
2017-05-06 22:07:46 +1000

    Fix booking tests

--- project/src/tests/BookingTest.java
+++ project/src/tests/BookingTest.java
@@ -6,6 +6,8 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 
 import org.junit.Before;
@@ -17,90 +19,53 @@ import model.ShiftTime;
 public class BookingTest
 {
 	public DateFormat dateFormat;
-	public Booking booking1, booking2, booking3, booking4;
+	public Booking booking1, booking1Clone, booking2, booking3, booking4;
 	
 	@Before
-	/*public void setUp() throws ParseException
+	public void setUp() throws ParseException
 	{
 		dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 		
 		// variables for constructor
 		int bookingID, employeeID;
 		String customer;
-		Date date;
-		ShiftTime time;
+		LocalDate date;
+		LocalTime time;
 		
 		// create some test objects
 		
 		bookingID = 1;
 		customer = "customer";
 		employeeID = 5;
-		date = dateFormat.parse("01-03-2017");
-		time = ShiftTime.MORNING;
+		date = LocalDate.of(2017, 3, 1);
+		time = LocalTime.of(10, 30);
 		
 		booking1 = new Booking(bookingID, customer, employeeID, date, time);
+		booking1Clone = new Booking(bookingID, customer, employeeID, date, time);
 		
 		bookingID = 2;
 		customer = "customer";
 		employeeID = 3;
-		date = dateFormat.parse("12-6-2017");
-		time = ShiftTime.EVENING;
+		date = LocalDate.of(2017, 6, 12);
+		time = LocalTime.of(17, 00);
 		
 		booking2 = new Booking(bookingID, customer, employeeID, date, time);
 		
 		bookingID = 3;
 		customer = "someoneelse";
 		employeeID = 7;
-		date = dateFormat.parse("12-6-2017");
-		time = ShiftTime.AFTERNOON;
+		date = LocalDate.of(2017, 6, 12);
+		time = LocalTime.of(14, 45);
 		
 		booking3 = new Booking(bookingID, customer, employeeID, date, time);
 		
 		bookingID = 3;
 		customer = "someoneelse";
 		employeeID = 7;
-		date = dateFormat.parse("21-2-2017");
-		time = ShiftTime.AFTERNOON;
+		date = LocalDate.of(2017, 2, 21);
+		time = LocalTime.of(14, 45);
 		
 		booking4 = new Booking(bookingID, customer, employeeID, date, time);
-	}
-	*/
-	
-	
-	@Test
-	public void testID()
-	{
-		assertEquals(booking1.ID, 1);
-	}
-
-	@Test
-	public void testCustomer()
-	{
-		assertEquals(booking1.getCustomer(), "customer");
-	}
-
-	@Test
-	public void testEmployeeID()
-	{
-		assertEquals(booking1.getEmployeeID(), 5);
-	}
-
-	@Test
-	public void testDate() throws ParseException
-	{
-		assertEquals(booking1.getDate(), dateFormat.parse("01-03-2017"));
-	}
-
-	@Test
-	public void testDay()
-	{
-		assertEquals(booking1.getDay(), DayOfWeek.WEDNESDAY);
-	}
-
-	@Test
-	public void testTime()
-	{
-		assertEquals(booking1.getTime(), ShiftTime.MORNING);
 	}
 	
 	@Test
@@ -124,6 +89,6 @@ public class BookingTest
 	@Test
 	public void testCompareEqual()
 	{
-		assertTrue(booking1.compareTo(booking1) == 0);
+		assertTrue(booking1.compareTo(booking1Clone) == 0);
 	}
 }
