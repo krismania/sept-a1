c537a72ef5d37fe483fa6413cccf3f1ccbc9628f
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-05-11 18:52:25 +1000

    Create businessDatabase class

---
+++ project/src/database/businessDatabase.java
@@ -0,0 +1,10 @@
+package database;
+
+public class businessDatabase extends Database{
+
+	public businessDatabase(String dbName) {
+		super(dbName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
