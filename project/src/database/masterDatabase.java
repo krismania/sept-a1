5e259d2ec2d0e0db7dcc7a7cdea90e52fb663d13
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-05-11 18:53:26 +1000

    Created masterDatabase class

---
+++ project/src/database/masterDatabase.java
@@ -0,0 +1,87 @@
+package database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import model.Booking;
+
+public class masterDatabase extends Database{
+
+	public masterDatabase(String dbName) {
+		super(dbName);
+	}
+	
+	/**
+	 * Returns an arrayList of all businessNames
+	 * Used for master DB
+	 * @author James
+	 */
+	public ArrayList<String> getAllBusinesses()
+	{
+		ArrayList<String> businessNames = new ArrayList<String>();
+		try
+		{
+			openConnection();
+			Statement stmt = c.createStatement();
+			
+			//JM Selected all constraints for a customer
+			String sql = "SELECT * FROM Businesses";
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next())
+			{
+		        //Retrieve by column name         
+	         	String name = rs.getString("BusinessName");
+
+				// build obj and add to list. -kg
+	         	businessNames.add(name);
+			}
+			closeConnection();
+		}
+		catch(SQLException e)
+		{
+			//JM Handle errors for JDBC
+		    logger.warning(e.toString());
+		}
+		catch(Exception e)
+		{
+		    //JM Handle errors for Class.forName
+			logger.warning(e.toString());
+		}
+		
+		return businessNames;
+	}
+	
+	@Override
+	protected void createTables()
+	{
+		logger.info("Creating master database tables...");
+
+		Table businesses = new Table("Businesses");
+		businesses.addColumn("businessName", "varchar(255)");
+		businesses.setPrimary("businessName");
+		
+		try
+		{
+			try (Statement stmt = c.createStatement())
+			{
+				// Customer Table
+				logger.fine("Creating table: " + businesses);
+				stmt.execute(businesses.toString());
+				insert("testDB");
+			}
+		}
+		catch (SQLException e)
+		{
+			logger.severe("SQL Exception in table creation: " + e);
+		}
+	}
+	
+	private boolean insert(String businessName)
+	{
+		return insert("Businesses", businessName);
+	}
+
+}
