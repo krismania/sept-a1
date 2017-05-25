56a41aeb47d371105fac4655b826a94d22726a0c
s3572290 <s3572290@student.rmit.edu.au>
2017-05-25 23:46:14 +1000

    method consolidation

--- project/src/gui/CreateBusinessController.java
+++ project/src/gui/CreateBusinessController.java
@@ -4,188 +4,176 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 import main.Controller;
 import main.Validate;
-//Implements Signup form for people without existing login details
-public class CreateBusinessController
-{
-	private Controller c = Controller.getInstance();
-	
-	@FXML private BorderPane root;
-	@FXML private TextField busName;
-	@FXML private TextField username;
-	@FXML private PasswordField password;
-	@FXML private PasswordField passwordConf;
-	
-	@FXML private TextField firstName;
-	@FXML private TextField lastName;
-	@FXML private TextField address;
-	@FXML private TextField phone;
-	
-	@FXML private Label lblError1;
-	@FXML private Label lblError2;
-	
-	//Helper method for triggering error output
-	private void setError(Label label, String text)
-	{
-		label.setText(text);
-		label.setVisible(true);
-	}
-	//Implements Generic helper method for switching between scenes
-	private void switchTo(String fxmlName)
-	{
-		try
-		{
-			// load the scene
-			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
-			
-			// get current stage
-			Stage stage = (Stage) root.getScene().getWindow();
-			
-			// switch scenes
-			stage.setScene(newScene);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	//Implements validation for new account data such as Password length and complexity
-	private boolean validateAccount()
-	{
-		// check the username and password
-		if (Validate.username(username.getText()))
-		{
-			if (Validate.password((password.getText())))
-			{
-				// check that confirm password matches
-				if (password.getText().equals(passwordConf.getText()))
-				{
-					lblError1.setVisible(false);
-					// username and password are accepted
-					return true;
-				}
-				else
-				{
-					setError(lblError1, "Passwords don't match.");
 
-					passwordConf.clear();
-					passwordConf.requestFocus();
-				}
-			}
-			else
-			{
-				// invalid password
-				// TODO: more detailed error
-				setError(lblError1, "Invalid Password.");
-				
-				password.clear();
-				passwordConf.clear();
-				password.requestFocus();
-			}
-		}
-		else
-		{
-			// invalid username
-			setError(lblError1, "Invalid Username.");
-			
-			username.requestFocus();
-		}
-		return false;
-	}
-	//Implements validation for new account data such as email address fields etc
-	private boolean validateDetails()
-	{
-		if(!busName.getText().isEmpty())
-		{
-			if (Validate.name(firstName.getText()))
-			{
-				if (Validate.name(lastName.getText()))
-				{
-					if (Validate.phone(phone.getText()))
-					{
-						lblError2.setVisible(false);
-						return true;
-					}
-					else
-					{
-						// invalid last name
-						// TODO: more detailed error
-						setError(lblError2, "Invalid phone number.");
-							
-						phone.requestFocus();
-					}
-				}
-			
-				else
-				{
-					// invalid last name
-					setError(lblError2, "Please enter a last name.");
-						
-					lastName.requestFocus();
-				}
-			}
-			else
-			{
-				// invalid first name
-				setError(lblError2, "Please enter a first name.");
-				
-				firstName.requestFocus();
-			}
-		}
-		else
-		{
-			// invalid business name
-			setError(lblError2, "Please enter a business name.");
-			
-			busName.requestFocus();
-		}
-		return false;
-	}
-	//Implements back button reverting to Login scene
-	@FXML
-	public void handleCancel(ActionEvent event) throws IOException
-	{
-		switchTo("AdminMenu");
-	}
-	//Accepts data input from fields and inserts them into db via controller class
-	@FXML
-	public void handleSignUp(ActionEvent event) throws IOException
-	{
-		// TODO: verify details
-		if (validateAccount() & validateDetails())
-		{
-			// try to sign up
-			boolean addBooking = c.addBusiness(username.getText(), password.getText(), firstName.getText(), 
-					lastName.getText(), address.getText(), phone.getText(), busName.getText());
-			
-			if(addBooking){
-				
-				// alert the user
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Account Created!");
-				alert.setHeaderText(null);
-				alert.setContentText("Account was successfully created.");
-				alert.showAndWait();
-				
-				switchTo("AdminMenu");
-			}
-			else
-			{
-				
-				// TODO: more specific errors here
-				setError(lblError2, "Account already exists.");
-			}
-		}
-		
-	}
+/**
+ * Implements Signup form for people without existing login details
+ *
+ * @author tn
+ *
+ */
+public class CreateBusinessController {
+    private Controller c = Controller.getInstance();
+
+    @FXML
+    private BorderPane root;
+    @FXML
+    private TextField busName;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField passwordConf;
+
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField address;
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private Label lblError1;
+    @FXML
+    private Label lblError2;
+
+    /**
+     * Helper method for triggering error output
+     *
+     * @param label
+     * @param text
+     * @author tn
+     */
+    private void setError(Label label, String text) {
+        label.setText(text);
+        label.setVisible(true);
+    }
+
+    /**
+     * Implements validation for new account data such as Password length and
+     * complexity
+     *
+     * @author tn
+     * @return
+     */
+    private boolean validateAccount() {
+        // check the username and password
+        if (Validate.username(username.getText())) {
+            if (Validate.password((password.getText()))) {
+                // check that confirm password matches
+                if (password.getText().equals(passwordConf.getText())) {
+                    lblError1.setVisible(false);
+                    // username and password are accepted
+                    return true;
+                } else {
+                    setError(lblError1, "Passwords don't match.");
+
+                    passwordConf.clear();
+                    passwordConf.requestFocus();
+                }
+            } else {
+                // invalid password
+                // TODO: more detailed error
+                setError(lblError1, "Invalid Password.");
+
+                password.clear();
+                passwordConf.clear();
+                password.requestFocus();
+            }
+        } else {
+            // invalid username
+            setError(lblError1, "Invalid Username.");
+
+            username.requestFocus();
+        }
+        return false;
+    }
+
+    /**
+     * Implements validation for new account data such as email address fields
+     * etc
+     *
+     * @author tn
+     */
+    private boolean validateDetails() {
+        if (!busName.getText().isEmpty()) {
+            if (Validate.name(firstName.getText())) {
+                if (Validate.name(lastName.getText())) {
+                    if (Validate.phone(phone.getText())) {
+                        lblError2.setVisible(false);
+                        return true;
+                    } else {
+                        // invalid last name
+                        // TODO: more detailed error
+                        setError(lblError2, "Invalid phone number.");
+
+                        phone.requestFocus();
+                    }
+                }
+
+                else {
+                    // invalid last name
+                    setError(lblError2, "Please enter a last name.");
+
+                    lastName.requestFocus();
+                }
+            } else {
+                // invalid first name
+                setError(lblError2, "Please enter a first name.");
+
+                firstName.requestFocus();
+            }
+        } else {
+            // invalid business name
+            setError(lblError2, "Please enter a business name.");
+
+            busName.requestFocus();
+        }
+        return false;
+    }
+
+    // Implements back button reverting to Login scene
+    @FXML
+    public void handleCancel(ActionEvent event) throws IOException {
+        GUIUtil.switchTo("AdminMenu", root);
+    }
+
+    // Accepts data input from fields and inserts them into db via controller
+    // class
+    @FXML
+    public void handleSignUp(ActionEvent event) throws IOException {
+        // TODO: verify details
+        if (validateAccount() & validateDetails()) {
+            // try to sign up
+            boolean addBooking = c.addBusiness(username.getText(), password.getText(), firstName.getText(),
+                    lastName.getText(), address.getText(), phone.getText(), busName.getText());
+
+            if (addBooking) {
+
+                // alert the user
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Account Created!");
+                alert.setHeaderText(null);
+                alert.setContentText("Account was successfully created.");
+                alert.showAndWait();
+
+                GUIUtil.switchTo("AdminMenu", root);
+            } else {
+
+                // TODO: more specific errors here
+                setError(lblError2, "Account already exists.");
+            }
+        }
+
+    }
 }
--- project/src/gui/CustMenuController.java
+++ project/src/gui/CustMenuController.java
@@ -8,16 +8,17 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.stage.Stage;
-import main.Controller;
-import javafx.scene.Node;
-import javafx.scene.Parent;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import main.Controller;
 
 /**
  * Implements main menu for Customer access level options
@@ -26,74 +27,83 @@ import javafx.scene.Scene;
  */
 public class CustMenuController implements Initializable {
 
-	private Controller c = Controller.getInstance();
-	
-	@FXML
-	private Button viewAvailCust;
-	
-	@FXML
-	private Button makeBooking;
-    
+    private Controller c = Controller.getInstance();
+
+    @FXML
+    private Button viewAvailCust;
+
+    @FXML
+    private Button makeBooking;
+
     @FXML
     private Button logout;
+
     /**
      * Initializes the controller class.
      */
-    //Implements on button action to proceed to Upcomming Appointments form
+    /**
+     * Implements on button action to proceed to Upcomming Appointments form
+     *
+     * @param event
+     * @throws IOException
+     * @author tn
+     */
     @FXML
     void handleButtonAction(ActionEvent event) throws IOException {
-       	//TN - get reference button stage
+        // TN - get reference button stage
         Stage stage = (Stage) viewAvailCust.getScene().getWindow();
-       	//TN - load other scene
+        // TN - load other scene
         Parent root = FXMLLoader.load(getClass().getResource("UpcomingAppointments.fxml"));
-   
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    //Implements logout button to exit to login
+
+    /**
+     * Implements logout button to exit to login
+     *
+     * @param event
+     * @throws IOException
+     * @author tn
+     */
     @FXML
     private void logoutButtonAction(ActionEvent event) throws IOException {
-    	c.logout();
-    	//reconnect to master DB
-		c.loadDatabase("master");
-    	
-    	Stage stage = (Stage) logout.getScene().getWindow();
-    	
-    	Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-    	   
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        c.logout();
+        // reconnect to master DB
+        c.loadDatabase("master");
+        GUIUtil.switchTo("Login.fxml", logout);
+
     }
-    //Implements On select button action navigating to bookings form
+
+    // Implements On select button action navigating to bookings form
     @FXML
     void handleBookingAction(ActionEvent event) throws IOException {
-       	//TN - get reference button stage
+        // TN - get reference button stage
         Stage stage = (Stage) makeBooking.getScene().getWindow();
-       	//TN - load other scene
+        // TN - load other scene
         Parent root = FXMLLoader.load(getClass().getResource("Booking.fxml"));
-   
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
+
     /**
      * Opens the ViewCustomerDetails screen
+     *
      * @author krismania
      */
     @FXML
-    public void handleViewDetails(ActionEvent event) throws IOException
-    {
-    	Node source = (Node) event.getSource();
-    	Stage stage = (Stage) source.getScene().getWindow();
-    	Parent root = FXMLLoader.load(getClass().getResource("ViewCustomerDetails.fxml"));
-    	
-    	Scene scene = new Scene(root);
-    	stage.setScene(scene);
+    public void handleViewDetails(ActionEvent event) throws IOException {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("ViewCustomerDetails.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // INIT
