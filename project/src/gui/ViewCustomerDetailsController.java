a1f9c1b253f8d148d8bef7512ca7747876280f1f
s3572290 <s3572290@student.rmit.edu.au>
2017-05-26 00:16:21 +1000

    Removed methods and replaced with Util class helpers

--- project/src/gui/ViewCustomerDetailsController.java
+++ project/src/gui/ViewCustomerDetailsController.java
@@ -1,80 +1,66 @@
 package gui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import database.model.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import main.Controller;
 
 /**
  * Displays the logged in customer's details.
+ * 
  * @author krismania
  */
-public class ViewCustomerDetailsController implements Initializable
-{
-	private Controller c = Controller.getInstance();
-	
-	@FXML private Node root;
-	
-	@FXML private Label username;
-	@FXML private TextField firstName;
-	@FXML private TextField lastName;
-	@FXML private TextField email;
-	@FXML private TextField phone;
-	
-	
-	/**
-     * Switches to a specified scene
-     * Implements Generic helper method for switching between scenes
-     * @author krismania
+public class ViewCustomerDetailsController implements Initializable {
+    private Controller c = Controller.getInstance();
+
+    @FXML
+    private Node root;
+
+    @FXML
+    private Label username;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phone;
+
+    /**
+     * Initialise data to populate fields
+     * 
+     * @author tn
      */
-    private void switchTo(String fxmlName)
-	{
-		try
-		{
-			// load the scene
-			Scene newScene = new Scene(FXMLLoader.load(getClass().getResource(fxmlName + ".fxml")));
-			
-			// get current stage
-			Stage stage = (Stage) root.getScene().getWindow();
-			
-			// switch scenes
-			stage.setScene(newScene);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	//Initialise data to populate fields
-	@Override
-	public void initialize(URL location, ResourceBundle resources)
-	{
-		// get user's account objects
-		Customer customer = (Customer) c.getLoggedUser();
-		
-		// populate the fields
-		username.setText(customer.username);
-		firstName.setText(customer.getFirstName());
-		lastName.setText(customer.getLastName());
-		email.setText(customer.getEmail());
-		phone.setText(customer.getPhoneNumber());
-	}
-	//Implements back button reverting to Customer Main menu scene
-	@FXML
-	public void handleBack(ActionEvent event)
-	{
-		switchTo("CustMenu");
-	}
-	
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // get user's account objects
+        Customer customer = (Customer) c.getLoggedUser();
+
+        // populate the fields
+        username.setText(customer.username);
+        firstName.setText(customer.getFirstName());
+        lastName.setText(customer.getLastName());
+        email.setText(customer.getEmail());
+        phone.setText(customer.getPhoneNumber());
+    }
+
+    /**
+     * Implements back button reverting to Customer Main menu scene
+     * 
+     * @param event
+     * @author tn
+     */
+    @FXML
+    public void handleBack(ActionEvent event) {
+        GUIUtil.switchTo("CustMenu", root);
+    }
+
 }
