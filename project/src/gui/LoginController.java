d99a9f05e94bf9cfef46972178d5c76b801ec00c
Kristian Giglia <krismania1@gmail.com>
2017-06-27 20:13:03 +1000

    Use switchTo on Login

--- project/src/gui/LoginController.java
+++ project/src/gui/LoginController.java
@@ -1,6 +1,5 @@
 package gui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,11 +7,8 @@ import database.model.Account;
 import database.model.Admin;
 import database.model.BusinessOwner;
 import database.model.Customer;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -20,7 +16,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 import main.Controller;
 
 /**
@@ -28,24 +23,19 @@ import main.Controller;
  * system app
  *
  * @author tn
+ * @author krismania
  *
  */
-public class LoginController implements Initializable {
+public class LoginController implements Initializable
+{
     Controller c = Controller.getInstance();
 
-    @FXML
-    private HBox root;
-    @FXML
-    private TextField tfUsername;
-    @FXML
-    private PasswordField tfPassword;
-    @FXML
-    private Label lblError;
-    @FXML
-    private ChoiceBox<String> businessPicker;
-
-    @FXML
-    private ImageView imageView;
+    @FXML private HBox root;
+    @FXML private TextField tfUsername;
+    @FXML private PasswordField tfPassword;
+    @FXML private Label lblError;
+    @FXML private ChoiceBox<String> businessPicker;
+    @FXML private ImageView imageView;
 
     /**
      * implements button action and decision logic for determining the users
@@ -56,55 +46,41 @@ public class LoginController implements Initializable {
      * @author tn
      */
     @FXML
-    public void handleLogin(ActionEvent event) throws IOException {
+    public void handleLogin()
+    {
+        // reset the error label before processing input
+        lblError.setVisible(false);
+
         // Disconnect from master DB
         c.disconnectMaster();
         // Load selected DB. JM
-        if (businessPicker.getValue() == "Administration") {
+        if (businessPicker.getValue() == "Administration")
+        {
             c.loadDatabase("master");
-        } else {
+        }
+        else
+        {
             c.loadDatabase(businessPicker.getValue());
         }
 
         // attempt login
         Account account = c.login(tfUsername.getText(), tfPassword.getText(), businessPicker.getValue());
 
-        if (account instanceof Customer) {
-            lblError.setVisible(false);
-
-            // load the scene
-            Scene customerLogin = new Scene(FXMLLoader.load(getClass().getResource("CustMenu.fxml")));
-
-            // get current stage
-            Stage stage = (Stage) root.getScene().getWindow();
-
-            // switch scenes
-            stage.setScene(customerLogin);
-        } else if (account instanceof BusinessOwner) {
-            lblError.setVisible(false);
-
-            // load the scene
-            Scene boLogin = new Scene(FXMLLoader.load(getClass().getResource("BOMenu.fxml")));
-
-            // get current stage
-            Stage stage = (Stage) root.getScene().getWindow();
-
-            // switch scenes
-            stage.setScene(boLogin);
-        } else if (account instanceof Admin) {
-            lblError.setVisible(false);
-
-            // load the scene
-            Scene adminLogin = new Scene(FXMLLoader.load(getClass().getResource("AdminMenu.fxml")));
-
-            // get current stage
-            Stage stage = (Stage) root.getScene().getWindow();
-
-            // switch scenes
-            stage.setScene(adminLogin);
-        } else {
-            // if account isn't an instance of either account type, login
-            // failed.
+        if (account instanceof Customer)
+        {
+            GUIUtil.switchTo("CustMenu", root);
+        }
+        else if (account instanceof BusinessOwner)
+        {
+            GUIUtil.switchTo("BOMenu", root);
+        }
+        else if (account instanceof Admin)
+        {
+            GUIUtil.switchTo("AdminMenu", root);
+        }
+        else
+        {
+            // if account isn't an instance of either account type, login failed.
             lblError.setVisible(true);
             // Remove attempted DB connect from memory in Controller.JM
             c.disconnectDB();
@@ -117,20 +93,12 @@ public class LoginController implements Initializable {
     /**
      * Implements button to direct new customers to the signup form
      *
-     * @param event
-     * @throws IOException
-     * @author tn
+     * @author krismania
      */
     @FXML
-    public void handleSignup(ActionEvent event) throws IOException {
-        // load the scene
-        Scene signup = new Scene(FXMLLoader.load(getClass().getResource("Signup.fxml")));
-
-        // get current stage
-        Stage stage = (Stage) root.getScene().getWindow();
-
-        // switch scenes
-        stage.setScene(signup);
+    public void handleSignup()
+    {
+        GUIUtil.switchTo("Signup", root);
     }
 
     /**
